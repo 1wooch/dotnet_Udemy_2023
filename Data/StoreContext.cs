54997f4b577c060a16e901fa8d50198d74ecd7d9
wwc <onechoo1106@gmail.com>
2024-02-19 22:24:07 +1000

    change the spacing

--- Data/StoreContext.cs
+++ Data/StoreContext.cs
@@ -7,9 +7,7 @@ namespace API.Data
     {
         public StoreContext(DbContextOptions options) : base(options)
         {
-
         }
-
         public DbSet<Product> Products { get; set; }
         public DbSet<Basket> Baskets { get; set; }
     }
