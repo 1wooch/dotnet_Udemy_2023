615ac75ae7e7ec91a47fafe1b698a40f2274ed98
wwc <onechoo1106@gmail.com>
2024-02-26 22:19:26 +1000

    axios api call problem for basket fixed

--- Controllers/BasketController.cs
+++ Controllers/BasketController.cs
@@ -4,100 +4,99 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+public class BasketController : BaseApiController
 {
-    public class BasketController:BaseApiController
+    private readonly StoreContext _context;
+    public BasketController(StoreContext context)
     {
-        private readonly StoreContext _context;
-        public BasketController(StoreContext context)
-        {
-            _context = context;
-        }
+        _context = context;
+    }
 
-        [HttpGet(Name ="GetBasket")]
-        public async Task<ActionResult<BasketDto>> GetBasket()
-        {
-            var basket = await RetrieveBasket();
-
-            if (basket == null) return NotFound();
-            return MapBasketToDto(basket);
-
-        }
-
-        
-
-        [HttpPost]
-        public async Task<ActionResult<BasketDto>> AddItemToBasket(int productId,int quantity){
-            
-            //1. Get Basket ->if it doesn't exist create one
-            var basket = await RetrieveBasket();
-            if(basket == null){
-                CreateBasket();
-            }
-
-            //2. get Product
-            var product = await _context.Products.FindAsync(productId); 
-            if(product == null) return NotFound();
-            
-            //3. Add product to basket
-            basket.AddItem(product,quantity);
-
-            //4. Save to db
-            var result = await _context.SaveChangesAsync()>0;
-            if(result)  return CreatedAtRoute("GetBasket",MapBasketToDto(basket));
-
-            return BadRequest(new ProblemDetails{Title="Problem adding item to basket"});
-        }
-
-       
-
-        [HttpDelete]
-        public async Task<ActionResult> DeleteItemFromBasket(int productId,int quantity){
-            var basket = await RetrieveBasket();
-            if(basket == null) return NotFound();
-     
-            basket.RemoveItem(productId,quantity);
-
-            var result = await _context.SaveChangesAsync()>0;
-            if(result)  return Ok();
-            return BadRequest(new ProblemDetails{Title="Problem removing item from basket"});
-        }   
-
-        private async Task<Basket> RetrieveBasket()
-        {
-            return await _context.Baskets
+    [HttpGet(Name = "GetBasket")]
+    public async Task<ActionResult<BasketDto>> GetBasket()
+    {
+        var basket = await _context.Baskets
             .Include(x => x.Items)
+            .ThenInclude(x => x.Product)
+            .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+
+        if (basket == null) return NotFound();
+
+        return MapBasketToDto(basket);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> AddItemToBasket(int productId, int quantity = 1)
+    {
+        var basket = await RetrieveBasket();
+
+        if (basket == null) basket = CreateBasket();
+
+        var product = await _context.Products.FindAsync(productId);
+
+        if (product == null) return NotFound();
+
+        basket.AddItem(product, quantity);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (result) return CreatedAtRoute("GetBasket", MapBasketToDto(basket));
+
+        return BadRequest(new ProblemDetails { Title = "Problem saving item to basket" });
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult> RemoveBasketItem(int productId, int quantity = 1)
+    {
+        var basket = await RetrieveBasket();
+
+        if (basket == null) return NotFound();
+
+        basket.RemoveItem(productId, quantity);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if (result) return Ok();
+
+        return BadRequest(new ProblemDetails { Title = "Problem removing item from the basket" });
+    }
+
+    private async Task<Basket> RetrieveBasket()
+    {
+        return await _context.Baskets
+            .Include(i => i.Items)
             .ThenInclude(p => p.Product)
-            .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]); 
-        }
-         private Basket CreateBasket()
+            .FirstOrDefaultAsync(basket => basket.BuyerId == Request.Cookies["buyerId"]);
+    }
+
+    private Basket CreateBasket()
+    {
+        var buyerId = Guid.NewGuid().ToString();
+        var cookieOptions = new CookieOptions { IsEssential = true, Expires = DateTime.Now.AddDays(30) };
+        Response.Cookies.Append("buyerId", buyerId, cookieOptions);
+        var basket = new Basket { BuyerId = buyerId };
+        _context.Baskets.Add(basket);
+        return basket;
+    }
+
+    private BasketDto MapBasketToDto(Basket basket)
+    {
+        return new BasketDto
         {
-            var buyerId=Guid.NewGuid().ToString();
-            var cookieOptions = new CookieOptions{IsEssential = true,Expires = DateTime.Now.AddDays(30)}; //this is how we create cookie option in .net core
-            Response.Cookies.Append("buyerId",buyerId,cookieOptions);
-            var basket = new Basket{BuyerId=buyerId};
-            _context.Baskets.Add(basket);
-            return basket;
-        }
-        private BasketDto MapBasketToDto(Basket basket)
-        {
-            return new BasketDto
+            Id = basket.Id,
+            BuyerId = basket.BuyerId,
+            Items = basket.Items.Select(item => new BsketItemDto
             {
-                Id = basket.Id,
-                BuyerId = basket.BuyerId,
-                Items = basket.Items.Select(x => new BsketItemDto
-                {
-                    ProductId = x.Product.Id,
-                    Name = x.Product.Name,
-                    Price = x.Product.Price,
-                    PictureUrl = x.Product.PictureUrl,
-                    Brand = x.Product.Brand,
-                    Type = x.Product.Type,
-                    Quantity = x.Quantity
-                }).ToList()
-
-            };
-        }
-
+                ProductId = item.ProductId,
+                Name = item.Product.Name,
+                Price = item.Product.Price,
+                PictureUrl = item.Product.PictureUrl,
+                Type = item.Product.Type,
+                Brand = item.Product.Brand,
+                Quantity = item.Quantity
+            }).ToList()
+        };
     }
 }
--- Entities/Basket.cs
+++ Entities/Basket.cs
@@ -1,36 +1,28 @@
+namespace API.Entities;
 
-namespace API.Entities
+public class Basket
 {
-    public class Basket
+    public int Id { get; set; }
+    public string BuyerId { get; set; }
+    public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+
+    public void AddItem(Product product, int quantity)
     {
-        public int Id{get;set;}
-        public string BuyerId{get;set;}
-        public List<BasketItem> Items{get;set;}=new List<BasketItem>();
-
-        public void AddItem(Product product, int quantity){
-            if(Items.All(x=>x.ProductId!=product.Id)){
-                Items.Add(new BasketItem{
-                    Product=product,
-                    Quantity=quantity
-                });
-            }
-            else{
-                var item=Items.FirstOrDefault(x=>x.ProductId==product.Id);
-                if(item!=null){
-                    item.Quantity+=quantity;    
-                }
-            }
+        if (Items.All(item => item.ProductId != product.Id))
+        {
+            Items.Add(new BasketItem { Product = product, Quantity = quantity });
+            return;
         }
 
-        public void RemoveItem(int productId,int quantity){
-            var item=Items.FirstOrDefault(x=>x.ProductId==productId);
-            if(item==null){
-               return;
-            }
-            item.Quantity-=quantity;
-            if(item.Quantity==0){
-                Items.Remove(item);
-            }
-        }
+        var existingItem = Items.FirstOrDefault(item => item.ProductId == product.Id);
+        if (existingItem != null) existingItem.Quantity += quantity;
+    }
+
+    public void RemoveItem(int productId, int quantity = 1)
+    {
+        var item = Items.FirstOrDefault(basketItem => basketItem.ProductId == productId);
+        if (item == null) return;
+        item.Quantity -= quantity;
+        if (item.Quantity == 0) Items.Remove(item);
     }
 }
