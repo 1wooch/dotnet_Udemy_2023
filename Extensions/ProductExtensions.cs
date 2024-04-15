ceafd6766c209435cb5821782fb01cbc2bdaefd7
wwc <onechoo1106@gmail.com>
2024-04-15 20:20:33 +1000

    add filter function #105

--- Extensions/ProductExtensions.cs
+++ Extensions/ProductExtensions.cs
@@ -26,5 +26,24 @@ namespace API.Extensions
 
             return query.Where(p => p.Name.ToLower().Contains(lowerCaseSearchTerm) || p.Description.ToLower().Contains(lowerCaseSearchTerm));
         }
+
+        public static IQueryable<Product> Filter(this IQueryable<Product> query, string brands, string types){
+            var brandList = new List<string>();
+            var typeList = new List<string>();
+
+            if(!string.IsNullOrEmpty(brands)){
+                brandList.AddRange(brands.ToLower().Split(',').Select(b => b.Trim()).ToList());
+            }
+
+            if(!string.IsNullOrEmpty(types)){
+                typeList.AddRange(types.ToLower().Split(',').Select(b => b.Trim()).ToList());
+            }
+
+            query =query.Where(p=> brandList.Count == 0 || brandList.Contains(p.Brand.ToLower()));
+            query = query.Where(p=> typeList.Count == 0 || typeList.Contains(p.Type.ToLower())); 
+
+            return query;
+
+        }
     }
 }
