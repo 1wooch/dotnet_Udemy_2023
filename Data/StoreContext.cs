d5eb529d872c4b19d17b42f6a447f98348e972c3
wwc <onechoo1106@gmail.com>
2024-05-15 20:49:12 +1000

    add nuget for entity identity and roles #123

--- Data/StoreContext.cs
+++ Data/StoreContext.cs
@@ -1,14 +1,28 @@
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity; // Add this line
+
 
 namespace API.Data
 {
-    public class StoreContext: DbContext
+    public class StoreContext: IdentityDbContext<User>
     {
         public StoreContext(DbContextOptions options) : base(options)
         {
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Basket> Baskets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().HasData(
+                //Set the Roles
+                new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" },
+                new IdentityRole { Name = "Member", NormalizedName = "MEMBER" }
+            )
+        }
     }
 }
---
+++ Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Entities
+{
+	public class User: IdentityUser
+	{
+		
+	}
+}
+
